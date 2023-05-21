7f3577c7c352a0a572e69de650b28982b29cadd0
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2023-05-21 17:22:58 +0400

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import "../src/assets/style/index.scss"
+
+function App() {
+  return<div className="App">
+      ULBI TV Start Onlayn Store
+    </div>
+
+}
+
+export default App;
